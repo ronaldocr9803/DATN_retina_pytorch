131a959924b5a7793b50d1c4cc2f5fb822ccaa79
root <ronaldocr9803@gmail.com>
2021-01-02 19:09:40 +0000

    minor change on bash script

--- train.sh
+++ train.sh
@@ -3,5 +3,6 @@ python3 train.py \
     --csv_train="dataset_not_aug/pascal_train.csv" \
     --csv_classes="dataset_not_aug/classes.csv" \
     --csv_val="dataset_not_aug/pascal_val.csv" \
-    --epochs=30 \
+    --epochs=50 \
+    --depth=101 \
     --weights_folder="./chkpoint_no_aug"
